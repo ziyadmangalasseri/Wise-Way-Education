fb5f10e4ec58fe67d6e1f62fbd7224a2910b408e
Abdul rahib <rahibrahib6740@gmail.com>
2025-03-20 16:04:23 +0530

    first setup commit

---
+++ components/ContactForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const contactForm = () => {
+  return (
+    <div className='flex flex-col'>
+        <button className=''>
+            CONTACT WITH US
+        </button>
+    </div>
+  )
+}
