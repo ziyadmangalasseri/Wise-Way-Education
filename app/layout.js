df5a5bca4acbcfc1fccc6f73bbb8bafdb5b0ea32
Abdul rahib <rahibrahib6740@gmail.com>
2025-03-20 22:58:28 +0530

    bg-setup

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}
-        <div>
+        <div className="bg-white">
 <Header/>
        
         {children}
