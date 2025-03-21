e25ed9ab4e67dc4c54acda31f8c5f7a8f7f69cab
muhammed ajnas tk <ajnastk432@gmail.com>
2025-03-21 16:46:18 +0530

    recycle

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,26 +7,29 @@ const Navbar = ({ isMenuOpen, toggleMenu }) => {
   const pathname = usePathname();
 
   return (
-    <nav className={`
+    <nav
+      className={`
       bg-white 
       w-full 
-      ${isMenuOpen ? 'h-full' : ''}
-      md:w-auto
-      lg:w-[890px] 
-      xl:w-[890px] 
+      ${isMenuOpen ? "h-full" : ""}
+     md:mx-32
+      lg:w-[950px] 
+      xl:w-[1150px] 
       md:h-[70px] 
       lg:h-[80px] 
       md:rounded-full 
       flex 
       items-center 
+      justify-center
       shadow-[0_-5px_10px_rgba(0,0,0,0.2),5px_0_15px_rgba(0,0,0,0.2)] 
       relative 
       z-10
-    `}>
-      <div className="w-full flex flex-col md:flex-row items-center md:items-stretch justify-between px-4 md:px-8 lg:px-12">
+    `}
+    >
+      <div className="w-full flex flex-col md:flex-row items-center justify-center md:justify-between px-4 md:px-8 lg:px-12">
         {/* Navigation Links */}
-        <div className="flex flex-col md:flex-row items-center md:items-start font-bold">
-          <ul className="flex flex-col md:flex-row text-base lg:text-lg gap-2 md:gap-1 lg:gap-3 items-center md:items-start">
+        <div className="flex flex-col md:flex-row items-center justify-center font-bold w-full">
+          <ul className="flex flex-col md:flex-row text-base lg:text-lg gap-2 md:gap-3 lg:gap-5 items-center md:items-start">
             {[
               { name: "Home", path: "/" },
               { name: "About", path: "/about" },
@@ -39,7 +42,7 @@ const Navbar = ({ isMenuOpen, toggleMenu }) => {
               <li key={link.path}>
                 <Link
                   href={link.path}
-                  className={`block py-3 md:py-4 px-2 md:px-2 lg:px-4 transition duration-200 relative text-center md:text-left ${
+                  className={`block py-3 md:py-4 px-2 md:px-3 lg:px-4 transition duration-200 relative text-center ${
                     pathname === link.path
                       ? "text-emerald-500 font-extrabold"
                       : "text-gray-800 hover:text-emerald-500"
@@ -56,15 +59,15 @@ const Navbar = ({ isMenuOpen, toggleMenu }) => {
           </ul>
         </div>
 
-        {/* Enquire Button - Fixed at the end of navbar
-        <div className="md:self-center mt-4 md:mt-0 mb-6 md:mb-0">
+        {/* Enquire Button */}
+        <div className="w-full md:mt-0 md:mb-0 flex justify-center">
           <Link
             href="/enquire"
-            className="bg-emerald-500 hover:bg-emerald-600 text-white text-lg md:text-xl lg:text-2xl font-bold py-2 sm:py-2 md:py-2 lg:py-4 px-8 lg:px-8 rounded-full transition duration-300 block text-center whitespace-nowrap"
+            className="bg-emerald-500 hover:bg-emerald-600 text-white text-lg md:text-xl lg:text-2xl font-bold py-2 sm:py-2 lg:py-4 px-8  lg:px-8 rounded-full transition duration-300 block text-center"
           >
             Enquire Now
           </Link>
-        </div> */}
+        </div>
       </div>
     </nav>
   );
